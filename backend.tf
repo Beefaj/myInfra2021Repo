c8d9d575e64250ab56f4eb2e8922aa71ed1587c0
Beefaj <149312476+Beefaj@users.noreply.github.com>
2024-01-08 18:23:55 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-fajol-tf-state-bucket"
+    bucket = "my-fajol-tf-state-bucket2"
     key = "main"
     region = "us-east-2"
     dynamodb_table = "my-dynamodb-table"
